732c970ef082c25ad45d53b628b3744d44f30ee9
nidhal <nidhal.jelassi@fsegt.utm.tn>
2023-04-11 10:15:54 +0100

    Initial commit

---
+++ app/src/main/java/com/tp4/gradesappfirebase/AddActivity.java
@@ -0,0 +1,84 @@
+package com.tp4.gradesappfirebase;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class AddActivity extends AppCompatActivity {
+
+    EditText editName;
+    EditText editnote1;
+    EditText editnote2;
+    EditText editnote3;
+    EditText editnote4;
+    EditText editnote5;
+    EditText editnote6;
+    DatabaseReference reff;
+
+    Student student;
+
+
+// String[] allTopics = {"Android", "Angular", "UX", "Databases", "C++", "Big Data"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        this.editName = findViewById(R.id.editName);
+        this.editnote1 = findViewById(R.id.editNoteAndroid);
+        this.editnote2 = findViewById(R.id.editNoteAngular);
+        this.editnote3 = findViewById(R.id.editNoteUX);
+        this.editnote4 = findViewById(R.id.editNoteDB);
+        this.editnote5 = findViewById(R.id.editNoteC);
+        this.editnote6 = findViewById(R.id.editNoteBigData);
+
+        Intent i = getIntent(); // facultatif sauf si y'a un extra ou un data
+
+        reff = FirebaseDatabase.getInstance().getReference().child(i.getStringExtra("child_name"));
+        student = new Student();
+
+
+    }
+
+    public void CommitData(View v) {
+
+        String name = editName.getText().toString();
+        float note1 = Float.parseFloat(editnote1.getText().toString());
+        float note2 = Float.parseFloat(editnote2.getText().toString());
+        float note3 = Float.parseFloat(editnote3.getText().toString());
+        float note4 = Float.parseFloat(editnote4.getText().toString());
+        float note5 = Float.parseFloat(editnote5.getText().toString());
+        float note6 = Float.parseFloat(editnote6.getText().toString());
+
+
+        student.setName(name);
+        student.setNoteAndroid(note1);
+        student.setNoteAngular(note2);
+        student.setNoteUX(note3);
+        student.setNoteDB(note4);
+        student.setNoteC(note5);
+        student.setNoteBigData(note6);
+
+
+        //V1 clé généré automatiquement par firebase
+        reff.push().setValue(student);
+
+        //V2 name = clé du document
+        reff.child(name).setValue(student);
+
+
+        Toast.makeText(getApplicationContext(), "Data inserted !", Toast.LENGTH_LONG).show();
+
+        finish();
+
+    }
+}
---
+++ app/src/main/java/com/tp4/gradesappfirebase/MainActivity.java
@@ -0,0 +1,169 @@
+package com.tp4.gradesappfirebase;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lst;
+    ArrayList<String> allStudents = new ArrayList<>();// = {"Sara", "Samira", "Sami"};
+    ArrayList<String> Notes = new ArrayList<>();
+    String[] allTopics = {"Android", "Angular", "UX", "Databases", "C++", "Big Data"};
+
+    String selectedStudent;
+
+    //1st Version
+//    String[] NotesSara = {"12", "7", "3.5", "15", "8", "10.5"};
+//    String[] NotesSamira = {"2", "4", "18.5", "16", "18", "5.75"};
+//    String[] NotesSami = {"11", "17", "5.5", "10", "2", "10.25"};
+
+    //2nd Version
+//    String[][] NotesAll = {
+//            {"12", "7", "3.5", "15", "8", "10.5"},
+//            {"2", "4", "18.5", "16", "18", "5.75"},
+//            {"11", "17", "5.5", "10", "2", "10.25"}
+//    };
+
+    AutoCompleteTextView autoSaisie;
+    DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.lst = this.findViewById(R.id.notesLst);
+
+        this.autoSaisie = findViewById(R.id.saisieAuto);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Student");
+
+
+
+        autoSaisie.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedStudent = ((TextView) view).getText().toString();
+
+                MyAdapter adapter;
+
+                getNotes(selectedStudent);
+
+                adapter = new MyAdapter(MainActivity.this, Notes);
+
+                lst.setAdapter(adapter);
+
+                lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                        String selectedTopic = allTopics[i];
+                        //TextView myNote = (TextView) view;
+                        TextView txt = (TextView) view.findViewById(R.id.note);
+                        Double note = Double.parseDouble(txt.getText().toString());
+
+
+
+                        if (note >= 10)
+                            Toast.makeText(getApplicationContext(), selectedTopic + ": Pass", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(getApplicationContext(), selectedTopic + ": Fail", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        getStudentsName();
+
+        ArrayAdapter ar = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, this.allStudents);
+
+        autoSaisie.setAdapter(ar);
+
+
+
+    }
+
+    public void goToAdd(View v) {
+        Intent i = new Intent(this, AddActivity.class);
+        i.putExtra("child_name", "Student");
+        startActivityForResult(i, 1);
+    }
+
+    private void getStudentsName() {
+        this.allStudents.clear();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot ds : snapshot.getChildren()) {
+
+                    Student s = ds.getValue(Student.class);
+
+                    allStudents.add(s.getName());
+
+                }
+
+                //Toast.makeText(InfosActivity.this, value, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(MainActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void getNotes(final String studentName) {
+        Notes.clear();
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                Student s = new Student();
+                s = snapshot.child(studentName).getValue(Student.class);
+
+                Notes.add(String.valueOf(s.getNoteAndroid()));
+                Notes.add(String.valueOf(s.getNoteAngular()));
+                Notes.add(String.valueOf(s.getNoteUX()));
+                Notes.add(String.valueOf(s.getNoteDB()));
+                Notes.add(String.valueOf(s.getNoteC()));
+                Notes.add(String.valueOf(s.getNoteBigData()));
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(MainActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
